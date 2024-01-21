a90e23c5831edaf6032f5c15746bfad2deb786be
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-01-21 22:38:28 +0200

    Implement visual design

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import employeesListItem from "../employees-list-item/employees-list-item";
+import './employees-list.css'
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    )
+}
+
+export default EmployeesList
